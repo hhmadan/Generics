659430538c621250312f13eba6558e6289d56751
hhmadan <hhmadan03@gmail.com>
2022-11-01 17:11:49 +0530

    ADDED: Created printMax method to print maximum using Generic Method

--- src/com/generics/GenericClass.java
+++ src/com/generics/GenericClass.java
@@ -1,33 +1,31 @@
 package com.generics;
 
-public class GenericClass<T extends Comparable<T>>{
-    public T firstElement, secondElement, thirdElement, fourthElement;
+        public class GenericClass<T extends Comparable<T>>{
+            public T firstElement, secondElement, thirdElement, fourthElement;
 
-    public GenericClass(T firstElement, T secondElement, T thirdElement, T fourthElement)
-    {
-        this.firstElement = firstElement;
-        this.secondElement = secondElement;
-        this.thirdElement = thirdElement;
-        this.fourthElement = fourthElement;
-    }
+            public GenericClass(T firstElement, T secondElement, T thirdElement, T fourthElement)
+            {
+                this.firstElement = firstElement;
+                this.secondElement = secondElement;
+                this.thirdElement = thirdElement;
+                this.fourthElement = fourthElement;
+            }
+            public static <T extends Comparable<T>> T getMaximum(T firstElement, T secondElement, T thirdElement, T fourthElement) {
 
-    public static <T extends Comparable<T>> T getMaximum(T firstElement, T secondElement, T thirdElement, T fourthElement) {
-
-        if (firstElement.compareTo(secondElement) >= 0 && firstElement.compareTo(thirdElement) >= 0 && firstElement.compareTo(fourthElement)>=0){
-            return firstElement;
+                T max = firstElement;
+                if(secondElement.compareTo(max)>0){
+                    max = secondElement;
+                }
+                if(thirdElement.compareTo(max)>0){
+                    max = thirdElement;
+                }
+                if(fourthElement.compareTo(max)>0){
+                    max = fourthElement;
+                }
+                printMax(firstElement,secondElement,thirdElement,fourthElement,max);
+                return max;
+            }
+            public static <T> void printMax(T firstElement, T secondElement, T thirdElement, T fourthElement, T max){
+                System.out.println("Maximum of "+firstElement+", "+secondElement+", "+thirdElement+", "+fourthElement+" is: "+max);
+            }
         }
-
-        if (secondElement.compareTo(firstElement) >= 0 && secondElement.compareTo(thirdElement) >= 0 && secondElement.compareTo(fourthElement)>=0) {
-            return secondElement;
-        }
-
-        if (thirdElement.compareTo(firstElement) >= 0 && thirdElement.compareTo(secondElement) >= 0 && thirdElement.compareTo(fourthElement)>=0) {
-            return thirdElement;
-        }
-        if (fourthElement.compareTo(firstElement) >= 0 && fourthElement.compareTo(secondElement) >= 0 && fourthElement.compareTo(thirdElement)>=0) {
-            return fourthElement;
-        }
-        return null;
-    }
-
-}
--- src/com/generics/MaximumWithGenerics.java
+++ src/com/generics/MaximumWithGenerics.java
@@ -4,12 +4,12 @@ public class MaximumWithGenerics {
     public static void main(String[] args) {
 
         Integer integer = GenericClass.getMaximum(31, 67, 32,89);
-        System.out.println("Maximum Integer is: "+integer);
+        //System.out.println("Maximum Integer is: "+integer);
 
         Float fl = GenericClass.getMaximum(31.2f, 62.2f, 3.2f,0.2f);
-        System.out.println("Maximum Float is: "+fl);
+        //System.out.println("Maximum Float is: "+fl);
 
         String string = GenericClass.getMaximum("Apple", "Peach", "Banana","Mango");
-        System.out.println("Maximum String is: "+string);
+        //System.out.println("Maximum String is: "+string);
     }
 }
